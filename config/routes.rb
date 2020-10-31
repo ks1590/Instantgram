8c150d8c6540f2177f8878318d8e6420defae638
kensuke suzuki <35k.0101@gmail.com>
2020-10-31 19:16:27 +0900

    ヘッダー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root "users#new"
+  resources :posts do
+		collection do   
+			post :confirm
+		end
+	end
 	resources :sessions, only: [:new, :create, :edit, :destroy]
 	resources :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_27_145727) do
+ActiveRecord::Schema.define(version: 2020_10_31_061634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
