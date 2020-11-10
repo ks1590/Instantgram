c09d9eeb7c52a2f946b07b6972a5ffebbc8aa583
kensuke suzuki <35k.0101@gmail.com>
2020-11-10 21:24:39 +0900

    新規登録、ログイン画面のアクセス制限を追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 class SessionsController < ApplicationController
   before_action :authenticate_user, {only: [:edit, :destroy]}
   def new
+    if logged_in?
+      redirect_to posts_path
+    end
   end
 
   def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
   
   def new
     @user = User.new
+    if logged_in?
+      redirect_to posts_path
+    end
   end
 
   def create
@@ -18,7 +21,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
     if @user != current_user
-        redirect_to user_path(current_user), alert: "不正なアクセスです。"
+      redirect_to user_path(current_user), alert: "不正なアクセスです。"
     end
   end
   
